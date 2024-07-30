81d8d8275813838f1566d31df97c0ee8cccd70bb
Козловский Иван <blackpuma072@gmail.com>
2024-07-30 20:22:01 +0300

    .

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Header from "@/app/components/Header";
 import LeftPanel from "@/app/components/LeftPanel.js";
 import CentralPanel from "@/app/components/CentralPanel.js";
 import RightPanel from "@/app/components/RightPanel.js";
@@ -6,7 +5,6 @@ import RightPanel from "@/app/components/RightPanel.js";
 export default function Home() {
   return (
     <>
-      <Header />
       <main className="container">
         <LeftPanel />
         <CentralPanel />
